b7ebe1c35fd81fe44822c7b31871ea1e9eb4ab06
Uladzislau Uhniavionak <uh.vladislav@gmail.com>
2026-05-12 09:21:52 +0200

    Add UserForm

--- src/UserForm.java
+++ src/UserForm.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class UserForm {
+public record UserForm(String email, String password, int age) {
+    public UserForm {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("Email cannot be blank");
+        }
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("Password cannot be blank");
+        }
+    }
 }
