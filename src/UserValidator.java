d27ee44df269de05a2934e6c7343f94a74abe927
Uladzislau Uhniavionak <uh.vladislav@gmail.com>
2026-05-12 09:22:18 +0200

    Add UserValidator

--- src/UserValidator.java
+++ src/UserValidator.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class UserValidator {
+    private final List<Predicate<UserForm>> rules = new ArrayList<>();
+
+    public void addRule(Predicate<UserForm> rule) {
+        rules.add(rule);
+    }
+
+    public boolean isValid(UserForm form) {
+        for (Predicate<UserForm> rule : rules) {
+            if (!rule.test(form)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
